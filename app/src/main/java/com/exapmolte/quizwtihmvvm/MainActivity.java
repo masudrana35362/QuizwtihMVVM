a20fa4ddbf6af76eaca97e0a1ec848d1cdf4516b
Md Masud Rana <masudrana35362@gmail.com>
2020-12-23 01:35:09 +0600

    Initial commit

---
+++ app/src/main/java/com/exapmolte/quizwtihmvvm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.exapmolte.quizwtihmvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
